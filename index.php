dd2bfb8c0ce87ae6fdf981e88b3d2d657d690fe6
LinnHtetAung87 <98522300+LinnHtetAung87@users.noreply.github.com>
2022-09-01 16:47:58 +0630

    Add files via upload

--- index.php
+++ index.php
@@ -1,197 +1,38 @@
-<?php
-include 'inc/header.php';
+<html>
+<head>
+    <title>Contact us form using php mysql</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Request Something or Submit feedback</h3>
+        <form action="form-process.php" method="POST">
+            <div class="form-group">
+                <label for="firstname">Firstname</label>
+                <input type="text" name="firstname" id="firstname" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="lastname">Lastname</label>
+                <input type="text" name="lastname" id="lastname" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input type="tel" name="phone" id="phone" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <input type="text" name="message" id="message" class="form-control" required>
+            </div>
+            <div class="form-group">
+        <button class="btn btn-success" type="submit">Submit</button>
+        <button class="btn btn-danger" type="reset">Reset</button>
+    </div>
+        </form>
+    </div>
+</body>
 
-Session::CheckSession();
-
-$logMsg = Session::get('logMsg');
-if (isset($logMsg)) {
-  echo $logMsg;
-}
-$msg = Session::get('msg');
-if (isset($msg)) {
-  echo $msg;
-}
-Session::set("msg", NULL);
-Session::set("logMsg", NULL);
-?>
-<?php
-
-if (isset($_GET['remove'])) {
-  $remove = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['remove']);
-  $removeUser = $users->deleteUserById($remove);
-}
-
-if (isset($removeUser)) {
-  echo $removeUser;
-}
-if (isset($_GET['deactive'])) {
-  $deactive = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['deactive']);
-  $deactiveId = $users->userDeactiveByAdmin($deactive);
-}
-
-if (isset($deactiveId)) {
-  echo $deactiveId;
-}
-if (isset($_GET['active'])) {
-  $active = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['active']);
-  $activeId = $users->userActiveByAdmin($active);
-}
-
-if (isset($activeId)) {
-  echo $activeId;
-}
-
-
- ?>
-      <div class="card ">
-        <div class="card-header">
-          <h3><i class="fas fa-users mr-2"></i>User list <span class="float-right">Welcome! <strong>
-            <span class="badge badge-lg badge-secondary text-white">
-<?php
-$username = Session::get('username');
-if (isset($username)) {
-  echo $username;
-}
- ?></span>
-
-          </strong></span></h3>
-        </div>
-        <div class="card-body pr-2 pl-2">
-
-          <table id="example" class="table table-striped table-bordered" style="width:100%">
-                  <thead>
-                    <tr>
-                      <th  class="text-center">SL</th>
-                      <th  class="text-center">Name</th>
-                      <th  class="text-center">Username</th>
-                      <th  class="text-center">Email address</th>
-                      <th  class="text-center">Mobile</th>
-                      <th  class="text-center">Status</th>
-                      <th  class="text-center">Created</th>
-                      <th  width='25%' class="text-center">Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php
-
-                      $allUser = $users->selectAllUserData();
-
-                      if ($allUser) {
-                        $i = 0;
-                        foreach ($allUser as  $value) {
-                          $i++;
-
-                     ?>
-
-                      <tr class="text-center"
-                      <?php if (Session::get("id") == $value->id) {
-                        echo "style='background:#d9edf7' ";
-                      } ?>
-                      >
-
-                        <td><?php echo $i; ?></td>
-                        <td><?php echo $value->name; ?></td>
-                        <td><?php echo $value->username; ?> <br>
-                          <?php if ($value->roleid  == '1'){
-                          echo "<span class='badge badge-lg badge-info text-white'>Admin</span>";
-                        } elseif ($value->roleid == '2') {
-                          echo "<span class='badge badge-lg badge-dark text-white'>Editor</span>";
-                        }elseif ($value->roleid == '3') {
-                            echo "<span class='badge badge-lg badge-dark text-white'>User Only</span>";
-                        } ?></td>
-                        <td><?php echo $value->email; ?></td>
-
-                        <td><span class="badge badge-lg badge-secondary text-white"><?php echo $value->mobile; ?></span></td>
-                        <td>
-                          <?php if ($value->isActive == '0') { ?>
-                          <span class="badge badge-lg badge-info text-white">Active</span>
-                        <?php }else{ ?>
-                    <span class="badge badge-lg badge-danger text-white">Deactive</span>
-                        <?php } ?>
-
-                        </td>
-                        <td><span class="badge badge-lg badge-secondary text-white"><?php echo $users->formatDate($value->created_at);  ?></span></td>
-
-                        <td>
-                          <?php if ( Session::get("roleid") == '1') {?>
-                            <a class="btn btn-success btn-sm
-                            " href="profile.php?id=<?php echo $value->id;?>">View</a>
-                            <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
-                            <a onclick="return confirm('Are you sure To Delete ?')" class="btn btn-danger
-                    <?php if (Session::get("id") == $value->id) {
-                      echo "disabled";
-                    } ?>
-                             btn-sm " href="?remove=<?php echo $value->id;?>">Remove</a>
-
-                             <?php if ($value->isActive == '0') {  ?>
-                               <a onclick="return confirm('Are you sure To Deactive ?')" class="btn btn-warning
-                       <?php if (Session::get("id") == $value->id) {
-                         echo "disabled";
-                       } ?>
-                                btn-sm " href="?deactive=<?php echo $value->id;?>">Disable</a>
-                             <?php } elseif($value->isActive == '1'){?>
-                               <a onclick="return confirm('Are you sure To Active ?')" class="btn btn-secondary
-                       <?php if (Session::get("id") == $value->id) {
-                         echo "disabled";
-                       } ?>
-                                btn-sm " href="?active=<?php echo $value->id;?>">Active</a>
-                             <?php } ?>
-
-
-
-
-                        <?php  }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '2'){ ?>
-                          <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
-                          <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
-                        <?php  }elseif( Session::get("roleid") == '2'){ ?>
-                          <a class="btn btn-success btn-sm
-                          <?php if ($value->roleid == '1') {
-                            echo "disabled";
-                          } ?>
-                          " href="profile.php?id=<?php echo $value->id;?>">View</a>
-                          <a class="btn btn-info btn-sm
-                          <?php if ($value->roleid == '1') {
-                            echo "disabled";
-                          } ?>
-                          " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
-                        <?php }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '3'){ ?>
-                          <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
-                          <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
-                        <?php }else{ ?>
-                          <a class="btn btn-success btn-sm
-                          <?php if ($value->roleid == '1') {
-                            echo "disabled";
-                          } ?>
-                          " href="profile.php?id=<?php echo $value->id;?>">View</a>
-
-                        <?php } ?>
-
-                        </td>
-                      </tr>
-                    <?php }}else{ ?>
-                      <tr class="text-center">
-                      <td>No user availabe now !</td>
-                    </tr>
-                    <?php } ?>
-
-                  </tbody>
-
-              </table>
-
-
-
-
-
-
-
-
-
-        </div>
-      </div>
-
-
-
-  <?php
-  include 'inc/footer.php';
-
-  ?>
+</html>
